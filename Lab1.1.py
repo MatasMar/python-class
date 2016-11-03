f9496475ae3f47d07bae2cf66c46ba5839decee3
MatasMar <matas.martisauskas@gmail.com>
2016-11-03 21:27:36 +0200

    trash

--- Lab1.1.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3 -tt
-row  = ' |'.join([' ']*3)
-collumn = ('-'*8)
-print (row)  
-print (collumn)
-print (row)
-print (collumn)
-print (row) # neveike sep komanda kazkodel
--- Lab1.2.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python3 -tt
-x = ' |'.join([' ']*3)
-y = '--+--+--'
-print (x)
-print (y)
-m = x 'H' x 'H' x
-print m
