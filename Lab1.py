c7847c3cdc7e140708c98b516f233241fa40c644
MatasMar <matas.martisauskas@gmail.com>
2016-11-03 21:18:01 +0200

    add tictactoe

--- Lab1.py
+++ Lab1.py
@@ -2,5 +2,15 @@
 def hello():
     print ('betkas')
 
+def tictactoe():
+    row  = ' |'.join([' ']*3)
+    collumn = ('-'*8)
+    print (row)
+    print (collumn)
+    print (row)
+    print (collumn)
+    print (row) # neveike sep komanda kazkodel
+
 if __name__ == "__main__":
     hello()
+    tictactoe()
