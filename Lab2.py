942290be81a3fb0db232f5017797e5c51cfce52b
MatasMar <matas.martisauskas@gmail.com>
2016-11-10 20:31:44 +0200

    lab2

---
+++ Lab2.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3 -tt
+from __future__ import division
+def gcd():
+    userInputA = int(input("number a:"))
+    userInputB = int(input("number b:"))
+    GCD = []
+    divisor = userInputA
+    print (userInputA / divisor)
+    while userInputA > 0:
+        if userInputA / divisor == userInputA % divisor and userInputB % 6 == type(int):
+            GCD.append(divisor)
+        divisor -= 1
+    print (GCD)
+if __name__ == "__main__":
+    gcd()
