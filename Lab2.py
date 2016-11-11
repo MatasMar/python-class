5cd8e3dc743b396b00bd3618b64392a199a495b7
MatasMar <matas.martisauskas@gmail.com>
2016-11-11 22:24:17 +0200

    add GCD

--- Lab2.py
+++ Lab2.py
@@ -1,15 +1,40 @@
 #!/usr/bin/env python3 -tt
-from __future__ import division
+
 def gcd():
     userInputA = int(input("number a:"))
     userInputB = int(input("number b:"))
     GCD = []
     divisor = userInputA
-    print (userInputA / divisor)
-    while userInputA > 0:
-        if userInputA / divisor == userInputA % divisor and userInputB % 6 == type(int):
+    while divisor != 0:
+        if userInputA % divisor == 0 and userInputB % divisor == 0:
             GCD.append(divisor)
         divisor -= 1
-    print (GCD)
+    print ('greatest common divisor:', max(GCD))
+
+def reverseDictionary():
+    dictionary = {}
+    values = []
+    newValue = 'y'
+    while newValue == 'y':
+        newValue = input('do you want to create a new dictionary value? (y for yes):')
+        if newValue == 'y':
+            userInput = input('dictionary item: ')
+            userinputValue = input('item value: ').split()
+            dictionary[userInput] = userinputValue
+            values.append(userInput)
+    print (values[0])
+    print (dictionary)
+    x = len(values)
+    y = 1
+    while y <= len(values):
+        dictionary[''.join(dictionary.get(values[len(values) - x]))] = values[len(values) - x]
+        del dictionary[values[len(values) - x]]
+        x += 1
+        y += 1
+    print (dictionary)
+
+
+
 if __name__ == "__main__":
     gcd()
+    #reverseDictionary()
